404f2d9dde0521820d811fc85fb5bec10f481788
Grigoryan Artem <artem.thebest@mail.ru>
2015-11-08 23:57:05 +0300

    Added class "Coordinate" and Added test for coordinate.

---
+++ EVOTests/ParametersTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EVO;
+using EVO.Parameters;
+
+namespace EVOTests
+{
+    [TestClass]
+    public class EVOTests
+    {
+        [TestMethod]
+        public void CoordinateArithmeticTest()
+        {
+            Coordinate a = new Coordinate(0, 0);
+            Coordinate b = new Coordinate(10, -15);
+            Coordinate c;
+
+            c = a + b;
+
+            Assert.AreEqual(new Coordinate(10, -15), c);
+
+            c = a - b;
+
+            Assert.AreEqual(new Coordinate(-10, 15), c);
+
+            c = -a;
+
+            Assert.AreEqual(a, c);
+
+            c = -b;
+            Assert.AreEqual(new Coordinate(-10, 15), c);
+        }
+    }
+}
