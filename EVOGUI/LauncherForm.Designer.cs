4342e7fad8c9f5185a66f662077ed3ec34844da2
Grigoryan Artem <artem.thebest@mail.ru>
2015-11-18 23:08:45 +0200

    Chenged window size

--- EVOGUI/LauncherForm.Designer.cs
+++ EVOGUI/LauncherForm.Designer.cs
@@ -39,10 +39,10 @@
             // 
             this.lTitle.AutoSize = true;
             this.lTitle.BackColor = System.Drawing.Color.Transparent;
-            this.lTitle.Font = new System.Drawing.Font("Ravie", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lTitle.Location = new System.Drawing.Point(12, 9);
+            this.lTitle.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTitle.Location = new System.Drawing.Point(19, 12);
             this.lTitle.Name = "lTitle";
-            this.lTitle.Size = new System.Drawing.Size(208, 86);
+            this.lTitle.Size = new System.Drawing.Size(137, 75);
             this.lTitle.TabIndex = 2;
             this.lTitle.Text = "EVO";
             this.lTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -51,7 +51,7 @@
             // 
             this.bStart.BackColor = System.Drawing.Color.White;
             this.bStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bStart.Location = new System.Drawing.Point(46, 155);
+            this.bStart.Location = new System.Drawing.Point(24, 136);
             this.bStart.Name = "bStart";
             this.bStart.Size = new System.Drawing.Size(132, 45);
             this.bStart.TabIndex = 3;
@@ -62,7 +62,7 @@
             // 
             this.bSettings.BackColor = System.Drawing.Color.White;
             this.bSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bSettings.Location = new System.Drawing.Point(46, 206);
+            this.bSettings.Location = new System.Drawing.Point(24, 187);
             this.bSettings.Name = "bSettings";
             this.bSettings.Size = new System.Drawing.Size(132, 45);
             this.bSettings.TabIndex = 4;
@@ -73,7 +73,7 @@
             // 
             this.bExit.BackColor = System.Drawing.Color.White;
             this.bExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.bExit.Location = new System.Drawing.Point(46, 257);
+            this.bExit.Location = new System.Drawing.Point(24, 238);
             this.bExit.Name = "bExit";
             this.bExit.Size = new System.Drawing.Size(132, 45);
             this.bExit.TabIndex = 5;
@@ -86,7 +86,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::EVOGUI.Properties.Resources.LauncherBackground;
-            this.ClientSize = new System.Drawing.Size(225, 318);
+            this.ClientSize = new System.Drawing.Size(178, 318);
             this.Controls.Add(this.bExit);
             this.Controls.Add(this.bSettings);
             this.Controls.Add(this.bStart);
