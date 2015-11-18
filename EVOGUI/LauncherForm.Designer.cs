a61bf00ed792849bd53bac13198fa3d4f780af3b
Grigoryan Artem <artem.thebest@mail.ru>
2015-11-18 22:49:09 +0200

    Added LauncherForm

---
+++ EVOGUI/LauncherForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace EVOGUI
+{
+    partial class LauncherForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LauncherForm));
+            this.lTitle = new System.Windows.Forms.Label();
+            this.bStart = new System.Windows.Forms.Button();
+            this.bSettings = new System.Windows.Forms.Button();
+            this.bExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lTitle
+            // 
+            this.lTitle.AutoSize = true;
+            this.lTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lTitle.Font = new System.Drawing.Font("Ravie", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lTitle.Location = new System.Drawing.Point(12, 9);
+            this.lTitle.Name = "lTitle";
+            this.lTitle.Size = new System.Drawing.Size(208, 86);
+            this.lTitle.TabIndex = 2;
+            this.lTitle.Text = "EVO";
+            this.lTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bStart
+            // 
+            this.bStart.BackColor = System.Drawing.Color.White;
+            this.bStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bStart.Location = new System.Drawing.Point(46, 155);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(132, 45);
+            this.bStart.TabIndex = 3;
+            this.bStart.Text = "Start";
+            this.bStart.UseVisualStyleBackColor = false;
+            // 
+            // bSettings
+            // 
+            this.bSettings.BackColor = System.Drawing.Color.White;
+            this.bSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bSettings.Location = new System.Drawing.Point(46, 206);
+            this.bSettings.Name = "bSettings";
+            this.bSettings.Size = new System.Drawing.Size(132, 45);
+            this.bSettings.TabIndex = 4;
+            this.bSettings.Text = "Settings";
+            this.bSettings.UseVisualStyleBackColor = false;
+            // 
+            // bExit
+            // 
+            this.bExit.BackColor = System.Drawing.Color.White;
+            this.bExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bExit.Location = new System.Drawing.Point(46, 257);
+            this.bExit.Name = "bExit";
+            this.bExit.Size = new System.Drawing.Size(132, 45);
+            this.bExit.TabIndex = 5;
+            this.bExit.Text = "Exit";
+            this.bExit.UseVisualStyleBackColor = false;
+            this.bExit.Click += new System.EventHandler(this.bExit_Click);
+            // 
+            // LauncherForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EVOGUI.Properties.Resources.LauncherBackground;
+            this.ClientSize = new System.Drawing.Size(225, 318);
+            this.Controls.Add(this.bExit);
+            this.Controls.Add(this.bSettings);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.lTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LauncherForm";
+            this.Text = "EVO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lTitle;
+        private System.Windows.Forms.Button bStart;
+        private System.Windows.Forms.Button bSettings;
+        private System.Windows.Forms.Button bExit;
+    }
+}
+
