6076c331cd5402c4c8754c1a3dec0e70e78bacc5
Grigoryan Artem <artem.thebest@mail.ru>
2015-11-21 15:50:51 +0200

    Added water tile painter

---
+++ EVOLibrary/Painters/ITilePainter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using EVO.Tiles;
+
+namespace EVO.Painters
+{
+    public interface ITilePainter
+    {
+        Bitmap Draw(Tile tile);
+    }
+}
---
+++ EVOLibrary/Painters/WaterTilePainter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using EVO.Tiles;
+using EVOLibrary;
+
+namespace EVO.Painters
+{
+    public class WaterTilePainter : ITilePainter
+    {
+        private const int IMAGE_SIZE = 256;
+
+        public Bitmap Draw(Tile tile)
+        {
+            Bitmap image = new Bitmap(IMAGE_SIZE, IMAGE_SIZE);
+
+            image = Textures.WaterTileTexture;
+
+            return image;
+        }
+    }
+}
