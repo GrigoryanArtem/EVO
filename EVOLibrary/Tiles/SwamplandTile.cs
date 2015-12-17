8579775a01d60df084e4d86ff11ed3eaedc45a6c
Grigoryan Artem <artem.thebest@mail.ru>
2015-12-17 02:15:12 +0200

    Added Swampland tile

---
+++ EVOLibrary/Tiles/SwamplandTile.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EVO.Parameters;
+using EVOLibrary;
+using EVO.Painters;
+
+namespace EVO.Tiles
+{
+    public class SwamplandTile : Tile
+    {
+        public SwamplandTile(Tile tile)
+            : this(new Coordinate(tile.Properties.Position))
+        { }
+
+        public SwamplandTile(Tile tile, Coordinate newCoordinate)
+            : this(newCoordinate)
+        { }
+
+        public SwamplandTile(Coordinate position)
+        {
+            _painter = new SwamplandTilePainter();
+            _name = MainStrings.SwamplandTileName;
+            _properties = new TileProperties(100, 5, position, 18);
+            _effects = new TilePropertiesForEffects();
+        }
+
+        public override Tile Copy()
+        {
+            return new SwamplandTile(this);
+        }
+
+        public override Tile Copy(Coordinate newCoordinate)
+        {
+            return new SwamplandTile(this, newCoordinate);
+        }
+
+        public override string ToString()
+        {
+            return MainStrings.SwamplandTileTitle;
+        }
+    }
+}
